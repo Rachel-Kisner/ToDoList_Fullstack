65b571b2b233e6eb0e0beb7175d6159b058dfcd7
“racheli” <racheli>
2025-02-10 16:38:04 +0200

    docker_2

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c0102ad184a4cdfb98b6e1a346cb60018607bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc974ddcdff6f699f25a5d82dec3987b763dd63")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
