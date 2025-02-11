e2e57cffa397e2c5df9f09b4bba16f2cf7a168ec
Rachel Kisner <rachel8508773@gmail.com>
2025-02-11 19:08:54 +0200

    kk

--- ToDoListReact-master/ToDoListReact-master/src/App.js
+++ ToDoListReact-master/ToDoListReact-master/src/App.js
@@ -41,6 +41,7 @@ function App() {
       </header>
       <section className="main" style={{ display: "block" }}>
         <ul className="todo-list">
+          if (Array.isArray(todos))
           {todos.map(todo => {
             return (
               <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
